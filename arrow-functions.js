c2855e2782e8d5886b9ccae28ab878503714e742
Anna Afijan <ann.afijan@gmail.com>
2024-05-26 18:21:32 +0000

    arrow functions

--- arrow-functions.js
+++ arrow-functions.js
@@ -4,11 +4,36 @@
  */
 
 // Vanilla JavaScript Function
-
+// function addTwoNumbers() {
+    // Code block
+//     return a + b;
+//     }
+// let sum = addTwoNumbers(3, 5);
+// console.log(sum);
 // Arrow Function With Parameters
-
+const addTwoNumbers = (a, b) => {
+    // Code block
+    return a + b;
+}
+let sum = addTwoNumbers(3, 5);
+console.log(sum);
 // Single Line Arrow Function With Parameters
-
+// const addTwoNumbers2 = (a, b) => (a + b); also valid
+const addTwoNumbers2 = (a, b) => a + b;
+let sum2 = addTwoNumbers2(6, 4);
+console.log(sum2);
 // Implicit Returns
+const saySomething = message => console.log(message);
+saySomething('Hallo there!!');
+
+const sayHello = () => console.log('hello'); 
+sayHello();
 
 // Returning Multiple Lines
+
+const returnMutipleLines = () => (
+    `<p>
+        This is multiple string!
+    </p>`
+)
+console.log(returnMutipleLines());
